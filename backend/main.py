d2410daef4a9dbb5e1044e52f1339d29143f4eaa
riteshdoibale <riteshdoibale@trendzlink.com>
2025-08-16 15:45:29 +0530

    separate api created for ollama and gpt models

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,29 @@
-# backend/main.py
+
 from fastapi import FastAPI
 import ollama
 from pydantic import BaseModel
 import openai
 
 app = FastAPI()
+
+
+class ChatRequest(BaseModel):
+    message: str
+    history: list[str] = []
+
+@app.post("/chat/ollama")
+async def chat(req: ChatRequest):
+    messages = []
+    for i, msg in enumerate(req.history):
+        role = "user" if i % 2 == 0 else "assistant"
+        messages.append({"role": role, "content": msg})
+    messages.append({"role": "user", "content": req.message})
+    # external call to ollama open source model
+    response = ollama.chat(model="llama2", messages=messages)
+    print("response",response)
+    reply = response['message']['content']
+    return {"reply": reply}
+
 
 openai.api_key = ""
 
@@ -24,21 +43,3 @@ async def chat(req: ChatRequest):
     )
     answer = response["choices"][0]["message"]["content"]
     return {"reply": answer}
-
-
-class ChatRequest(BaseModel):
-    message: str
-    history: list[str] = []
-
-@app.post("/chat/ollama")
-async def chat(req: ChatRequest):
-    messages = []
-    for i, msg in enumerate(req.history):
-        role = "user" if i % 2 == 0 else "assistant"
-        messages.append({"role": role, "content": msg})
-    messages.append({"role": "user", "content": req.message})
-    # external call to ollama open source model
-    response = ollama.chat(model="llama2", messages=messages)
-    print("response",response)
-    reply = response['message']['content']
-    return {"reply": reply}
